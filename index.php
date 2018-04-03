c86081054ae09d49ea1178459bf8c68b91766702
KachiAlex <37791815+KachiAlex@users.noreply.github.com>
2018-04-03 20:20:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 
 <html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
@@ -34,8 +34,8 @@
 
 		<section class="main">
 			<div class="get-started">
-                <h1>Onyedikachi Akoma <span class="for">and I am a </span> Favcoder</h1>
-				<p>My mission for Favcode is to:  Develop and deploy enterprise software products with innovation and creativity.</p>
+                <h1>My name is Onyedikachi Akoma <span class="for">and I am a </span> Favcoder</h1>
+				<p>My mission for Favcode is to:  Develop and deploy enterprise software products with innovation and creativity providing global solutions to world problems.</p>
                 
                 <h2>This is why you should choose me</h2>
                 <p><b>My name is Onyedikachi Akoma. I wish to apply to work with you. Although am relatively new to programming, but I am a quick and passionate learner. I can easily outdo my past achievement and am completely focused on being better than I was a minute ago. I have innovative ideas especially in coding. Working with you will change my life in a magnifold way. This is my dream and my reality. I believe I have the innovation and skill to be your greatest recruit. My only weakness is poor color combination but I have so much strength especially in thinking up new ideas that could be highly profiting and I quickly complete any task I am given. Here is a link to my Linkedin profile  Thank you for this opportunity</b>.</p>
